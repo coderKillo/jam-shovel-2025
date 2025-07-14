7cff43ad6d756ce3baff54d93f9c7390db80118c
chmu7568 <christian.mueller@esolutions.de>
2025-07-14 10:29:37 +0200

    finish player

--- game/entities/player/player.gd
+++ game/entities/player/player.gd
@@ -1,62 +1,145 @@
 class_name Player
 extends CharacterBody2D
 
-enum PlayerState { IDLE, ENGINE_ON, ENGINE_OFF, ENGINE_STARTED }
+enum PlayerState { IDLE, ENGINE_ON, ENGINE_OFF, ENGINE_STARTED, OVERHEAT }
+
+@onready var sprite: Sprite2D = $Sprite2D
 
 const JUMP_VELOCITY = -400.0
 
 const SPEED = 300.0
 const ACCELERATION = 400.0
+const DEACCELERATION = 400.0
 const MAX_SPEED = 800.0
 
-const TACHO_CHANGE_RATE = 1.0
+const TACHO_CHANGE_RATE = 10.0
 const TACHO_MAX = 5.0
 
-var _current_state := PlayerState.IDLE
+const HEAT_BUILD_BOOST = 30.0
+const HEAT_BUILD_NORMAL = 10.0
+const HEAT_BUILD_KILL = 5.0
+const HEAT_MAX = 100.0
+const HEAT_RESET_PERCENT = 25.0
 
-var _heat := 0.0
+var _current_state := PlayerState.IDLE
+var _current_tacho_change_rate := TACHO_CHANGE_RATE
+
+var _heat := 0.0:
+	set = _set_heat
+
+var _tacho := 0.0:
+	set = _set_tacho
+
 var _speed := 0.0
-var _tacho := 0.0
 
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	print("%s" % _current_state)
+	_update_rotation()
 
 	match _current_state:
 		PlayerState.IDLE:
-			_current_state = PlayerState.ENGINE_OFF
+			_init_player()
 
 		PlayerState.ENGINE_OFF:
 			if Input.is_action_just_pressed("jump"):
-				_speed = 0
-				_current_state = PlayerState.ENGINE_STARTED
+				_start_engine(delta)
 			else:
-				_speed -= ACCELERATION * delta
+				_break(delta)
 
 		PlayerState.ENGINE_STARTED:
-			if Input.is_action_just_pressed("jump"):
-				_speed = (_tacho / TACHO_MAX) * MAX_SPEED
-				_current_state = PlayerState.ENGINE_ON
+			if Input.is_action_just_pressed("jump") or _tacho < 0.0:
+				_launch(delta)
 			else:
-				_tacho += delta * TACHO_CHANGE_RATE
+				_starting(delta)
 
 		PlayerState.ENGINE_ON:
 			if Input.is_action_pressed("move_right") and is_on_floor():
-				_speed += ACCELERATION * delta
+				_move_normal(delta)
 			else:
-				_speed = lerp(_speed, SPEED, delta)
+				_move_boost(delta)
 
 			if Input.is_action_just_pressed("jump") and is_on_floor():
-				velocity.y = JUMP_VELOCITY
-				_current_state = PlayerState.ENGINE_OFF
+				_jump(delta)
 
+		PlayerState.OVERHEAT:
+			_overheat(delta)
 		_:
 			pass
 
+	print(_speed)
 	_speed = clamp(_speed, 0, MAX_SPEED)
 	velocity.x = _speed
 
 	move_and_slide()
+
+	## Handle Player State
+
+
+func _init_player():
+	_current_state = PlayerState.ENGINE_OFF
+
+
+func _start_engine(_delta):
+	_tacho = 0.0
+	_current_tacho_change_rate = TACHO_CHANGE_RATE
+	_current_state = PlayerState.ENGINE_STARTED
+
+
+func _break(delta):
+	_speed -= DEACCELERATION * delta
+
+
+func _launch(_delta):
+	_tacho = clamp(_tacho, 0.0, TACHO_MAX)
+	_speed = (_tacho / TACHO_MAX) * MAX_SPEED
+	_current_state = PlayerState.ENGINE_ON
+	Engine.time_scale = 1.0
+
+
+func _starting(delta):
+	_tacho += delta * _current_tacho_change_rate
+	Engine.time_scale = 0.6
+
+
+func _move_normal(delta):
+	_speed += ACCELERATION * delta
+	_heat += HEAT_BUILD_BOOST * delta
+
+
+func _move_boost(delta):
+	_heat += HEAT_BUILD_NORMAL * delta
+	_speed = lerp(_speed, SPEED, delta)
+
+
+func _jump(_delta):
+	velocity.y = JUMP_VELOCITY
+	_current_state = PlayerState.ENGINE_OFF
+
+
+func _overheat(_delta):
+	print("LOSE!!!")
+
+	## end Handle Player State
+
+
+func _set_tacho(value):
+	_tacho = value
+	if _tacho > TACHO_MAX:
+		_current_tacho_change_rate = -TACHO_CHANGE_RATE
+
+
+func _set_heat(value):
+	_heat = value
+	if _heat > HEAT_MAX:
+		_current_state = PlayerState.OVERHEAT
+	return _heat
+
+
+func _update_rotation():
+	if not is_on_floor() and _current_state == PlayerState.ENGINE_OFF:
+		sprite.rotation_degrees = -40
+	else:
+		sprite.rotation_degrees = 0
--- game/world/level.gd
+++ game/world/level.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var camera: Camera2D = $Camera2D
 @onready var player: Player = $Player
 @onready var tacho_bar: ProgressBar = $CanvasLayer/GUI/ProgressBar
+@onready var heat_bar: ProgressBar = $CanvasLayer/GUI/ProgressBar2
+
+const CAMERA_MAX_ZOOM = 0.7
+const CAMERA_MIN_ZOOM = 2.0
 
 
 func _ready():
@@ -11,6 +15,11 @@ func _ready():
 
 func _process(_delta):
 	camera.global_position = player.global_position
+	var zoom = Vector2.ONE * clamp(player.SPEED / player._speed, CAMERA_MAX_ZOOM, CAMERA_MIN_ZOOM)
+	camera.zoom = lerp(camera.zoom, zoom, 0.5)
 
 	tacho_bar.max_value = player.TACHO_MAX
 	tacho_bar.value = player._tacho
+
+	heat_bar.max_value = player.HEAT_MAX
+	heat_bar.value = player._heat
