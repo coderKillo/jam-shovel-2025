3e6cc5191ce3bf2aa28ba7ae645f7227973aabc3
chmu7568 <christian.mueller@esolutions.de>
2025-07-14 23:31:19 +0200

    improve zombies

--- game/entities/enemy/enemy.gd
+++ game/entities/enemy/enemy.gd
@@ -2,17 +2,61 @@ class_name Enemy
 extends CharacterBody2D
 
 const SPEED = 30.0
+const VARIANTS = 4
+
+const ACTIONS = [
+	"attack1",
+	"attack2",
+	"scream",
+]
+
+const DEATHS = [
+	"death1",
+	"death2",
+	"death3",
+	"death4",
+]
 
 signal died
 
 @onready var blood_splash_scene = preload("res://game/effects/blood/blood_splash.tscn")
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+
+var _varient = 0
+var _action_running := false
+var _speed = 0
+
+
+func _ready():
+	_varient = randi_range(1, VARIANTS)
+	animation.animation_looped.connect(_on_animation_looped)
 
 
 func _physics_process(delta):
 	velocity += get_gravity() * delta
 
-	velocity.x = -SPEED
+	velocity.x = -_speed
 	move_and_slide()
+
+	if not _action_running:
+		_update_animation()
+
+
+func _update_animation():
+	if (randi() % 100) <= 10:
+		_action_running = true
+		var action = ACTIONS.pick_random()
+		_play_animation(action)
+	else:
+		_play_animation("walk")
+
+
+func _play_animation(animation_name: String):
+	animation.play("%s_%s" % [animation_name, _varient])
+
+
+func _on_animation_looped():
+	_action_running = true
 
 
 func kill():
@@ -21,5 +65,9 @@ func kill():
 	var blood_splash = blood_splash_scene.instantiate()
 	get_parent().add_child(blood_splash)
 	blood_splash.position = global_position
+
+	set_physics_process(false)
+	_play_animation(DEATHS.pick_random())
+	await animation.animation_looped
 
 	queue_free()
---
+++ game/world/zombie_spawner.gd
@@ -0,0 +1,33 @@
+extends TileMapLayer
+
+const SPAWN_DISTANCE = 500.0
+
+@export var player: Player
+
+@onready var zombie_scene = preload("res://game/entities/enemy/zombie.tscn")
+
+var _spawn_positions: Array[Vector2]
+var _spawn_index: int
+
+
+func _ready():
+	for cell in get_used_cells():
+		_spawn_positions.append(map_to_local(cell))
+
+	_spawn_positions.sort()
+	_spawn_index = 0
+
+
+func _process(_delta):
+	if _spawn_index >= _spawn_positions.size():
+		return
+
+	if _spawn_positions[_spawn_index].distance_to(player.global_position) < SPAWN_DISTANCE:
+		_spawn(_spawn_positions[_spawn_index])
+		_spawn_index += 1
+
+
+func _spawn(spawn_position: Vector2):
+	var zombie = zombie_scene.instantiate()
+	add_child(zombie)
+	zombie.global_position = spawn_position
